fbdd21e168c1e6d5ea10acaeba50bb96c9709b59
arnabbiswas1 <arnab.biswas1@olacabs.com>
2022-07-20 11:44:22 +0530

    How to use methods

---
+++ src/components/Methods.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>Company-{{getCompany("Geospoc(Ola)")}}</h1>
+<h2>Age-{{getAge().age}}</h2>
+</template>
+
+<script>
+export default {
+    name: "MethodKey",
+    methods: {
+
+        getCompany(company) {
+            return company
+        },
+        getAge(){
+        return {
+            dob:"14/03/2000",
+            age:22,
+        }
+    },
+    },
+    
+}
+</script>
